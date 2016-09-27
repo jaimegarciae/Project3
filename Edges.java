58efe0d08ec49ec33cb1396752954c864ebebe9e
rstehle <regan.stehle@utexas.edu>
2016-09-27 15:21:14 -0500

    Update Edges.java

--- Edges.java
+++ Edges.java
@@ -1,5 +1,22 @@
+/* WORD LADDER Main.java
+ * EE422C Project 3 submission by
+ * Katya Malyavina
+ * ym5356
+ * 16465
+ * Regan Stehle 
+ * rms3762
+ * 16465
+ * Slip days used: 0
+ * Git URL: https://github.com/kmalyavina/Project3
+ * Fall 2016
+ */
+
 package assignment3;
-
+/**
+ * 
+ * @author rstehle
+ * Linked List of all edges, may not be necessary to have this as a separate class
+ */
 public class Edges {
-
+	//build linked list of edges by scanning dictionary for each word
 }
