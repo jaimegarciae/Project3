c1f429e34fa8a456f6760f028abab1a209aceaa1
rstehle <regan.stehle@utexas.edu>
2016-09-27 15:20:40 -0500

    Update Node.java

--- Node.java
+++ Node.java
@@ -1,5 +1,29 @@
+/* WORD LADDER Main.java
+ * EE422C Project 3 submission by
+ * Katya Malyavina
+ * ym5356
+ * 16465
+ * Regan Stehle 
+ * rms3762
+ * 16465
+ * Slip days used: 0
+ * Git URL: https://github.com/kmalyavina/Project3
+ * Fall 2016
+ */
+
 package assignment3;
 
-public class Node {
+import java.util.*;
 
+/**
+ * 
+ * @author rstehle
+ *Each word in dictionary becomes a Node, to create a graph where edges c
+ *connect words that are separated by one letter
+ */
+public class Node {
+	int distance; //for BFS & DFS, marked as visited/unvisited
+	//possibly just create a linked list of edges as an element here, to keep as an adjacency list
+	LinkedList<Node> edges; //if this is the way to parameterize this linked list
+	
 }
