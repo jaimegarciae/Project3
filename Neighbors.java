c9e3f93b95d8c33325d36ed3e51d1b2feae41c0c
rstehle <regan.stehle@utexas.edu>
2016-09-27 15:45:57 -0500

    Update and rename Edges.java to Neighbors.java

--- Neighbors.java
+++ Neighbors.java
@@ -1,4 +1,4 @@
-/* WORD LADDER Main.java
+/* WORD LADDER Neighbors.java
  * EE422C Project 3 submission by
  * Katya Malyavina
  * ym5356
@@ -17,6 +17,7 @@ package assignment3;
  * @author rstehle
  * Linked List of all edges, may not be necessary to have this as a separate class
  */
-public class Edges {
+public class Neighbors {
 	//build linked list of edges by scanning dictionary for each word
+	//this function currently in the constructor of Nodes but can be moved to separate class if necessary 
 }
