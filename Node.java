0f4e65a843886ab7658acef563a7a06465c6656c
rstehle <regan.stehle@utexas.edu>
2016-09-29 01:39:37 -0500

    Update Node.java

--- Node.java
+++ Node.java
@@ -39,23 +39,26 @@ public class Node {
 		visited = false;
 		neighbors = new LinkedList<String>(); 
 		
-		/* for(int i=0; i< word.length() ; i++){
+		for(int i=0; i< word.length() ; i++){
 			StringBuilder tempWord = new StringBuilder(word); //we can use tempWord to modify each letter in the word and 
 									//compare it against dictionary entries
 			for(char c = 'a'; c <= 'z'; c++){ //be aware everything in dictiojnary is upper case
 				if(tempWord.charAt(i) == c )
 					continue;
 				tempWord.setCharAt(i, c);
-				boolean neighbor = dictionary.contains(tempWord.toString().toUpperCase()); //words is Set<String> dictionary, must pass parameter in
+				boolean neighbor = Main.dictionary.contains(tempWord.toString().toUpperCase()); //words is Set<String> dictionary, must pass parameter in
 				//if neighbor = true, then need to add this word to LinkedList of neighbors
 				if(neighbor == true){
 					neighbors.add(tempWord.toString());
 					}
 				}
 		}
-	 */
 	}
 				
 			
 			
 			
+		
+	
+	
+}
